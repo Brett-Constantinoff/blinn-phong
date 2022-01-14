cfd1af504524afaffafc9ecbab0a7946a15c9cf9
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-01-14 10:44:45 -0700

    added source code for creation of ImGui UI window

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <math.h>
 
 #include "../shaders/shader.h"
 
@@ -38,6 +39,11 @@ int main(){
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io; //sets up input / output
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(win, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
 
     /* GLEW INIT */
     glewExperimental = true;
@@ -163,6 +169,7 @@ int main(){
         0.0f, 0.5f, 0.5f,
     };
 
+    //buffer creations
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
@@ -182,28 +189,67 @@ int main(){
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
 
+    bool main_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    glm::vec3 rotation_vec(0.0f, 1.0f, 0.5f);
+
     /* MAIN RENDER LOOP */
     while(!glfwWindowShouldClose(win)){
 
+        //loop variables
+        float rotation_angle = glm::radians(45.0f);
+        static float speed_factor = 0.5f;
+        float rotation_speed = (float)glfwGetTime() * rotation_angle * speed_factor;
         glm::mat4 model = glm::mat4(1.0f);
 
-        float rotation_angle = glm::radians(45.0f);
-        float rotation_speed = (float)glfwGetTime() * rotation_angle;
+        //input / output
+        glfwPollEvents();
 
-        glClearColor(0.40f, 0.47f, 0.70f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //setup imgui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
+        //main demo window
+        {   
+            ImGui::Begin("Hello, world!");                         
+
+            ImGui::Text("This is some useful text.");               
+            ImGui::Checkbox("Demo Window", &main_window);      
+            ImGui::Checkbox("Another Window", &main_window);
+
+            ImGui::SliderFloat3("Rotation", &rotation_vec.x, 0.0f, (2.0f * M_PI));            
+            ImGui::SliderFloat("Rotation Speed", &speed_factor, 0.0f, 1.0f);
+            ImGui::ColorEdit3("Background Color", (float*)&clear_color); 
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        //shaders
         glUseProgram(my_shader.ID);
         glBindVertexArray(VAO);
 
-        model = glm::rotate(model, rotation_speed, glm::vec3(0.0f, 1.0f, 0.5f));
+        //rendering 
+        model = glm::rotate(model, rotation_speed, rotation_vec);
         glUniformMatrix4fv(glGetUniformLocation(my_shader.ID, "model"), 1, GL_FALSE, &model[0][0]);
 
+        ImGui::Render();
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glDrawArrays(GL_TRIANGLES, 0, sizeof(vertex_positions) / 3);    
-
         glfwSwapBuffers(win); //swap front and back buffer
-        glfwPollEvents();
+      
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(win);
+    glfwTerminate();
 
     return 0;
 }
